5fef942c6a9b389c91cdcb468c4d914de31b2fbb
Mauro Delgado <maurods@uamv.edu.ni>
2025-10-28 22:23:22 -0600

    Add project files.

---
+++ Imagensita/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Imagensita
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnLPuerta = new System.Windows.Forms.Button();
+            this.btnRPuerta = new System.Windows.Forms.Button();
+            this.Hora = new System.Windows.Forms.Timer(this.components);
+            this.Tick = new System.Windows.Forms.Timer(this.components);
+            this.lblHora = new System.Windows.Forms.Label();
+            this.lblBonnieDangerLvl = new System.Windows.Forms.Label();
+            this.lblChicaDangerLvl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLPuerta
+            // 
+            this.btnLPuerta.Location = new System.Drawing.Point(140, 221);
+            this.btnLPuerta.Name = "btnLPuerta";
+            this.btnLPuerta.Size = new System.Drawing.Size(75, 23);
+            this.btnLPuerta.TabIndex = 0;
+            this.btnLPuerta.Text = "Abierto";
+            this.btnLPuerta.UseVisualStyleBackColor = true;
+            this.btnLPuerta.Click += new System.EventHandler(this.btnLPuerta_Click);
+            // 
+            // btnRPuerta
+            // 
+            this.btnRPuerta.Location = new System.Drawing.Point(530, 221);
+            this.btnRPuerta.Name = "btnRPuerta";
+            this.btnRPuerta.Size = new System.Drawing.Size(75, 23);
+            this.btnRPuerta.TabIndex = 1;
+            this.btnRPuerta.Text = "Abierto";
+            this.btnRPuerta.UseVisualStyleBackColor = true;
+            this.btnRPuerta.Click += new System.EventHandler(this.btnRPuerta_Click);
+            // 
+            // Hora
+            // 
+            this.Hora.Tick += new System.EventHandler(this.Hora_Tick);
+            // 
+            // Tick
+            // 
+            this.Tick.Tick += new System.EventHandler(this.Tick_Tick);
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.Location = new System.Drawing.Point(294, 67);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(33, 13);
+            this.lblHora.TabIndex = 2;
+            this.lblHora.Text = "12am";
+            // 
+            // lblBonnieDangerLvl
+            // 
+            this.lblBonnieDangerLvl.AutoSize = true;
+            this.lblBonnieDangerLvl.Location = new System.Drawing.Point(140, 174);
+            this.lblBonnieDangerLvl.Name = "lblBonnieDangerLvl";
+            this.lblBonnieDangerLvl.Size = new System.Drawing.Size(13, 13);
+            this.lblBonnieDangerLvl.TabIndex = 3;
+            this.lblBonnieDangerLvl.Text = "0";
+            // 
+            // lblChicaDangerLvl
+            // 
+            this.lblChicaDangerLvl.AutoSize = true;
+            this.lblChicaDangerLvl.Location = new System.Drawing.Point(592, 174);
+            this.lblChicaDangerLvl.Name = "lblChicaDangerLvl";
+            this.lblChicaDangerLvl.Size = new System.Drawing.Size(13, 13);
+            this.lblChicaDangerLvl.TabIndex = 4;
+            this.lblChicaDangerLvl.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblChicaDangerLvl);
+            this.Controls.Add(this.lblBonnieDangerLvl);
+            this.Controls.Add(this.lblHora);
+            this.Controls.Add(this.btnRPuerta);
+            this.Controls.Add(this.btnLPuerta);
+            this.Name = "Form1";
+            this.Text = "Hola";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLPuerta;
+        private System.Windows.Forms.Button btnRPuerta;
+        private System.Windows.Forms.Timer Hora;
+        private System.Windows.Forms.Timer Tick;
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.Label lblBonnieDangerLvl;
+        private System.Windows.Forms.Label lblChicaDangerLvl;
+    }
+}
+
---
+++ Imagensita/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Imagensita
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+
+        int Actividad = 0;
+
+        int ChanceMovimientoBonnie = 0, ChanceMovimientoChica = 0;
+
+        bool isRightDoorClosed = false, isLeftDoorClosed = false;        
+
+        int BonnieDangerLvl = 0, ChicaDangerLvl = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Hora.Interval = (30) * 1000;
+            Hora.Tick += Hora_Tick;
+            Hora.Tick -= Hora_Tick;
+            Hora.Start();
+
+            Tick.Interval = (1) * 1000;
+            Tick.Tick += Tick_Tick;
+            Tick.Tick -= Tick_Tick;
+            Tick.Start();
+
+            ChanceMovimientoBonnie = random.Next(1, 6);
+            ChanceMovimientoChica = random.Next(1, 6);
+        }
+
+        private void Hora_Tick(object sender, EventArgs e)
+        {
+            Actividad++;
+
+            lblHora.Text = Actividad.ToString() + "am";
+        }
+
+        private bool Bonnie(ref int num)
+        {
+            if (num >= 5)
+            {
+                num = random.Next(1, 6);
+                return true;
+            }
+
+            num++;
+            return false;
+
+        }
+
+        private bool Chica(ref int num)
+        {
+            if (num >= 5)
+            {
+                num = random.Next(1, 6);
+                return true;
+            }
+
+            num++;
+            return false;
+
+        }
+
+        private void Tick_Tick(object sender, EventArgs e)
+        {
+            if (Bonnie(ref ChanceMovimientoBonnie))
+            {
+                BonnieDangerLvl++;
+                lblBonnieDangerLvl.Text = BonnieDangerLvl.ToString();
+
+                if (BonnieDangerLvl == 6)
+                {
+                    if (!isLeftDoorClosed)
+                    {
+                        lblHora.Text = "Te culearon";
+
+                        Tick.Stop();
+                        Hora.Stop();
+
+                        var temporizadorCerrar = new System.Windows.Forms.Timer();
+                        temporizadorCerrar.Interval = 3000;
+                        temporizadorCerrar.Tick += (s, ev) =>
+                        {
+                            temporizadorCerrar.Stop();
+                            Close();
+                        };
+
+                        temporizadorCerrar.Start();
+                    }
+
+                    BonnieDangerLvl = 0;
+                }
+            }
+
+            if (Actividad < 1) return;
+
+            if (Chica(ref ChanceMovimientoChica))
+            {
+                ChicaDangerLvl++;
+                lblChicaDangerLvl.Text = ChicaDangerLvl.ToString();
+
+                if (ChicaDangerLvl == 6)
+                {
+                    if (!isRightDoorClosed)
+                    {
+                        lblHora.Text = "Te culearon";
+
+                        Tick.Stop();
+                        Hora.Stop();
+
+                        var temporizadorCerrar = new System.Windows.Forms.Timer();
+                        temporizadorCerrar.Interval = 3000;
+                        temporizadorCerrar.Tick += (s, ev) =>
+                        {
+                            temporizadorCerrar.Stop();
+                            Close();
+                        };
+
+                        temporizadorCerrar.Start();
+                    }
+
+                    ChicaDangerLvl = 0;
+                }
+
+                if (Actividad < 2) return;
+            }
+        }
+
+        private void btnLPuerta_Click(object sender, EventArgs e)
+        {
+            if (!isLeftDoorClosed)
+            {
+                isLeftDoorClosed = true;
+                btnLPuerta.Text = "Cerrado";
+
+                return;
+            }
+
+            isLeftDoorClosed = false;
+            btnLPuerta.Text = "Abierto";
+        }
+
+        private void btnRPuerta_Click(object sender, EventArgs e)
+        {
+            if (!isRightDoorClosed)
+            {
+                isRightDoorClosed = true;
+                btnRPuerta.Text = "Cerrado";
+
+                return;
+            }
+
+            isRightDoorClosed = false;
+            btnRPuerta.Text = "Abierto";
+        }
+    }
+}
